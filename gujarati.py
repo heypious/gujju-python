99440d5577ce452c737833f2dcf2b587344a0fd0
Param <59928150+heypious@users.noreply.github.com>
2022-11-24 19:21:39 +0530

    Add files via upload

---
+++ gujarati.py
@@ -0,0 +1,23 @@
+def dekhad(x):
+    print(x)
+
+def lambu(x):
+    print(len(x))
+
+def khol(x):
+    open(x)
+
+def ajubaju(x, y):
+    print(round(x, y))
+
+def prakar(x):
+    print(type(x))
+
+def aagar(x):
+    next(x)
+
+def undhu(x):
+    reversed(x)
+
+def undhu():
+    reversed()
---
+++ test.py
@@ -0,0 +1,10 @@
+from gujarati import *
+
+dekhad("Kemcho?")
+
+bhaibandh = ["Ramlo", "Premlo", "Gadhedo"]
+lambu(bhaibandh)
+
+marks = 69.82
+prakar(marks)
+
